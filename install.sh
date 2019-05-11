8cbce0e8dc0c5a8d7e93585518870ab08f5ec9d7
forvaidya <forvaidya@gmail.com>
2019-05-11 18:01:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,10 @@ cat<<EOF>~/.gerrit_helper_rc
 
 if [[ -z \$GERRIT_HELPERS_INSTALLED ]]
 then 
-      echo "Helpers installing now.."
+     
       PATH=${CURR_PATH}:\${PATH}
-      chmod -v a+rx ./git-*
+      chmod  a+rx ./git-*
       export GERRIT_HELPERS_INSTALLED=true
-      echo "Helpers installation completed.."
       :
 fi 
 EOF

118f3c7e8383be4eae6cc074be3db23a3ef3f163
forvaidya <forvaidya@gmail.com>
2019-05-13 11:52:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CURR_PATH=$(dirname $(readlink -f $0))
-(cd ${CURR_PATH} && git pull --rebase) 
+
+unset GERRIT_HELPERS_INSTALLED
 #FINAL_PATH=${CURR_PATH}:${PATH}
 #FINAL_PATH=$(printf %s "$FINAL_PATH" | awk -v RS=: -v ORS=: '!arr[$0]++')
 
@@ -17,7 +18,7 @@ then
       :
 fi 
 EOF
-
+(cd ${CURR_PATH} && git pull --rebase) 
 sed -i '/.gerrit_helper_rc/d' ~/.bashrc
 echo  ". ~/.gerrit_helper_rc"  >> ~/.bashrc 
 . ~/.bashrc 
